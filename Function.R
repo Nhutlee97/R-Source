5e2a5dfb2a056617907edcf9e3f13ad8017aedc7
Nhutlee97 <43366001+Nhutlee97@users.noreply.github.com>
2020-08-23 10:49:13 +0700

    Add files via upload

---
+++ Function.R
@@ -0,0 +1,23 @@
+# Calculate BMI
+cal_BMI <- function(height, weight, result){
+    BMI <- weight/(height^2)
+    result <- BMI
+    output <- c(height, weight, result)
+    return(output)
+}
+
+# Calculate sum_even
+sum_even.function <- function(start, end){
+    sum_even <- 0
+    for(i in start:end){
+        if(i%%2 == 0){
+            sum_even <- sum_even + i
+        }
+    }
+    return(sum_even)
+}
+
+# Function drink
+drink.function <- function(price, type = "Tea"){
+    print(paste("With", price, ", you can drink",type))
+}
