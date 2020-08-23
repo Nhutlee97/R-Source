50c0e4eb1dccd91dff9c7e25c6c5a2c638c748ba
Nhutlee97 <43366001+Nhutlee97@users.noreply.github.com>
2020-08-23 11:20:36 +0700

    Add files via upload

--- Function.R
+++ Function.R
@@ -21,3 +21,36 @@ sum_even.function <- function(start, end){
 drink.function <- function(price, type = "Tea"){
     print(paste("With", price, ", you can drink",type))
 }
+
+# Function tinh_tien_dien
+tinh_tien_dien_ver2020 <- function(so_Kwh){
+    muc_1 <- 1678
+    muc_2 <- 1734
+    muc_3 <- 2014
+    muc_4 <- 2536
+    muc_5 <- 2834
+    muc_6 <- 2927
+    
+    bac_50 <- 50
+    bac_100 <- 100
+    tien_dien <- 0
+    if(so_Kwh <= 50){
+        tien_dien <- so_Kwh*muc_1
+        return(tien_dien)
+    }else if(so_Kwh <= 100){
+        tien_dien <- bac_50*muc_1 + (so_Kwh-bac_50)*muc_2
+        return(tien_dien)
+    }else if(so_Kwh <= 200){
+        tien_dien <- bac_50*(muc_1 + muc_2) + (so_Kwh-bac_100)*muc_3
+        return(tien_dien)
+    }else if(so_Kwh <= 300){
+        tien_dien <- bac_50*(muc_1 + muc_2) + bac_100*muc_3 + (so_Kwh - bac_50 - bac_50 -bac_100)*muc_4
+        return(tien_dien)
+    }else if(so_Kwh <= 400){
+        tien_dien <- bac_50*(muc_1 + muc_2) + bac_100*(muc_3 + muc_4) + (so_Kwh - bac_50 - bac_50 - bac_100 - bac_100)*muc_5
+        return(tien_dien)
+    }else{
+        tien_dien <- bac_50*(muc_1 + muc_2) + bac_100*(muc_3 + muc_4 + muc_5) + (so_Kwh - bac_50 - bac_50 - bac_100 - bac_100 - bac_100)*muc_6
+        return(tien_dien)
+    }
+}
